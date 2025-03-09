e705d0da5b8f16664119bfb36ce9d3a748a95807
Cmhanz99 <hanzhmagbal@gmail.com>
2025-03-09 21:04:18 +0800

    Add files via upload

---
+++ DigitalClock/script.js
@@ -0,0 +1,27 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minutes');
+const second = document.getElementById('seconds');
+const amPM = document.getElementById('ampm');
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = 'AM';
+
+    if(h > 12){
+        h = h - 12;
+        ampm = 'PM';
+    }
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+    amPM.innerText = ampm;
+
+    setTimeout(() => {
+    updateClock();
+    }, 1000);
+}
+
+updateClock();
